06531516feda2a69431ab007cd6837fb8a943c59
unknown <bril_00@bk.ru>
2023-01-25 09:29:49 +0100

    components added

---
+++ my_app/src/components/Nav/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <div>
+        <div>
+            <img src="" alt="" />
+            <p>Catalog</p>
+        </div>
+        <div>
+            <p>Categories</p>
+            <p>Coupon</p>
+            <p>Stock</p>
+            <p>Contacts</p>
+            {/* icon */}
+        </div>
+    </div>
+  )
+}
